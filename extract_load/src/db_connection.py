feeb82ee38053803e44e9ff1db36834e053ea97f
banquo <banquo@banquos-MacBook-Pro.local>
2021-08-30 10:28:36 +0100

    created config file

--- extract_load/src/db_connection.py
+++ extract_load/src/db_connection.py
@@ -1,17 +1,14 @@
 import snowflake.connector
-import os
-from dotenv import load_dotenv
 
-
-load_dotenv() 
+from src.config import config
 
 
 def get_snowflake_connection():
     ctx = snowflake.connector.connect(
-        account=os.environ['snow_account'],
-        user=os.environ['snow_user'],
-        password=os.environ['snow_password'],
-        database=os.environ['snow_database'], 
-        schema=os.environ['snow_schema']
+        account=config['account'],
+        user=config['user'],
+        password=config['password'],
+        database=config['database'], 
+        schema=config['schema']
     )
     return ctx
