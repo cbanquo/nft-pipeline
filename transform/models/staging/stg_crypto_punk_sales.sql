7a7f25252ce34b4f40db82de344761c1775daa54
banquo <banquo@banquos-mbp.home>
2021-09-02 21:45:41 +0100

    better crypto punks subgraph

--- transform/models/staging/stg_crypto_punk_sales.sql
+++ transform/models/staging/stg_crypto_punk_sales.sql
@@ -15,10 +15,10 @@ formatted AS (
 
     SELECT 
         -- FK 
-        data:to:id::TEXT AS buyer_account_id, 
-        data:from:id::TEXT AS seller_account_id, 
+        data:buyer:id::TEXT AS buyer_account_id, 
+        data:seller:id::TEXT AS seller_account_id, 
 
-        'CRYPTO-PUNK-' || SPLIT_PART(data:id ,'-', 1)::TEXT AS token_id,
+        'CRYPTO-PUNK-' || data:nft:tokenId::TEXT AS token_id,
 
         '0x0000000000000000000000000000000000000000'::TEXT AS payment_token_id,
 
@@ -26,9 +26,9 @@ formatted AS (
         TO_TIMESTAMP(data:timestamp) AS block_at,
 
         -- Details
-        data:blockNumber::INT AS block_number,
+        data:block::INT AS block_number,
 
-        data:contract:name::TEXT AS project_name, 
+        'Crypto Punks' AS project_name, 
         'Larva Labs' AS artist_name,
 
         'Single' AS sale_type,
