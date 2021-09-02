59222a54ee679b49c03e0db93d60fa303016a64b
banquo <banquo@banquos-mbp.home>
2021-09-02 22:45:57 +0100

    ingets BAYC

--- ingest/main.py
+++ ingest/main.py
@@ -1,7 +1,8 @@
-from src.pipelines import ArtBlocksSalesPipeline, CryptoPunkSalesPipeline, NounsDaoAuctionPipeline
+from src.pipelines import ArtBlocksSalesPipeline, CryptoPunkSalesPipeline, NounsDaoAuctionPipeline, BAYCTransfersPipeline
 
 
 if __name__ == "__main__":
     ArtBlocksSalesPipeline().run()
     CryptoPunkSalesPipeline().run()
     NounsDaoAuctionPipeline().run()
+    BAYCTransfersPipeline().run()
