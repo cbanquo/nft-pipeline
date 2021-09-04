01fc62e4d016b6164db69ae8a89f4d8d88b5e9c4
banquo <banquo@banquos-MacBook-Pro.local>
2021-09-04 16:17:00 +0100

    crypto punk stg

---
+++ transform/models/staging/stg_crypto_punk_transfers.sql
@@ -0,0 +1,39 @@
+WITH crypto_punk_sales AS (
+
+    SELECT 
+        *
+    FROM 
+        {{ source('raw_python', 'crypto_punk_transfers')}}
+
+),
+
+/*
+    Cleaning
+*/
+
+formatted AS (
+
+    SELECT 
+        -- FK 
+        data:to:id::TEXT AS buyer_account_id, 
+        data:from:id::TEXT AS seller_account_id, 
+        'CRYPTO-PUNK-' || data:nft:tokenId::TEXT AS token_id,
+
+        -- Timestamps
+        TO_TIMESTAMP(data:timestamp) AS block_at,
+        data:block::INT AS block_number,
+
+        -- Details
+        'Transfer' AS transfer_type,
+        'Crypto Punks' AS project_name, 
+        'Larva Labs' AS artist_name,
+
+        -- Numbers
+        0 AS price
+        
+    FROM 
+        crypto_punk_sales
+
+)
+
+SELECT * FROM formatted
