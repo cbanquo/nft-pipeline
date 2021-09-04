74be5abaff2108badff1fefddee4972ff4e69e93
banquo <banquo@banquos-MacBook-Pro.local>
2021-09-04 15:38:22 +0100

    bayc staging

---
+++ transform/models/staging/stg_bayc_transfers.sql
@@ -0,0 +1,44 @@
+
+/*
+    Tables
+*/
+
+WITH transfers AS (
+
+    SELECT 
+        *
+    FROM 
+        {{ source('raw_python', 'bayc_transfers') }}
+
+),
+
+/*
+    Cleaing
+*/
+
+formatted AS (
+
+    SELECT
+        -- FK
+        data:to:id::TEXT AS buyer_account_id, 
+        data:from:id::TEXT AS seller_account_id,
+
+        'BAYC-' || data:token:id::TEXT AS token_id,
+
+        -- Timestamps
+        TO_TIMESTAMP(data:timestamp) AS block_at, 
+        
+        -- Details
+        data:block::INT AS block_number,
+
+        'Bored Ape Yacht Club'::TEXT AS project_name,
+        NULL AS artist_name,
+        
+        NULL AS price
+
+    FROM
+        transfers
+
+)
+
+SELECT * FROM formatted
